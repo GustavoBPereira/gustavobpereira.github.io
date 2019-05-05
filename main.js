8f0fbf232f0b5c333b3f74fda17c89154a5e169d
gustavobpereira <britopereiragustavo@gmail.com>
2019-05-04 21:52:13 -0300

    adição da opção de usar cartas para religiões

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let eventos = [];
 let primeira = true;
+let religiao = false;
+
 function start(){
     if(primeira == false){
         decisao = confirmarRecomeco();
@@ -7,14 +9,19 @@ function start(){
             return;
         }
     }
-    eventos = inciarDeck();
+    if (document.getElementById("religiao").checked == true){
+        religiao = true;
+    }else {
+        religiao = false;
+    }
+    eventos = inciarDeck(religiao);
     cartasRestantes();
     limparTela();
     primeira = false;
 }
 
-function inciarDeck(){
-    return[
+function inciarDeck(religiao){
+    deck = [
         ['Cartas não respondidas', 'A ação Ajuda Externa não pode ser efetuada durante esse turno'],
         ['Cartas não respondidas', 'A ação Ajuda Externa não pode ser efetuada durante esse turno'],
         ['Um dia ruim', 'A ação Renda não pode ser efetuada durante esse turno'],
@@ -31,27 +38,34 @@ function inciarDeck(){
         ['Perdendo aliados', 'Nenhuma habilidade de personagem pode ser utilizada durante esse turno'],
         ['Favores de um pirata', 'Você pode roubar uma moeda de outro jogador'],
         ['Favores de um pirata', 'Você pode roubar uma moeda de outro jogador'],
-        //['Informações privilegiadas', 'Você pode olhar a próxima carta de evento e escolher entre mantê-la ou coloca-la no fundo do baralho'],
-        //['Informações privilegiadas', 'Você pode olhar a próxima carta de evento e escolher entre mantê-la ou coloca-la no fundo do baralho'],
-        //['Conhecimento é poder', 'Você pode escolher um evento da pilha de descartes e imediatamente efetuá-lo'],
-        //['Conhecimento é poder', 'Você pode escolher um evento da pilha de descartes e imediatamente efetuá-lo'],
         ['Chantagens', 'Você pode nomear outro jogador e dar-lhe uma opção: 1-Pagar duas moedas a você para manter suas influências 2-Trocar uma influência'],
         ['Chantagens', 'Você pode nomear outro jogador e dar-lhe uma opção: 1-Pagar duas moedas a você para manter suas influências 2-Trocar uma influência'],
         ['Rede de mentiras', 'Todos os jogadores devem embaralhar uma de suas influências aptas ao montante e seguindo a ordem dos turnos, comprar uma nova'],
-        ['Planos sabotados', 'Todas as influências aptas dos jogadores devem ser embaralhadas (formando um montante separado) e redistribuídas, seguindo a ordem dos turnos'],
+        ['Planos sabotados','Todas as influências aptas dos jogadores devem ser embaralhadas (formando um montante separado) e redistribuídas, seguindo a ordem dos turnos'],
         ['O caos é uma escada', 'O(s) jogador(es) com a maior riqueza deve(m) pagar duas moedas para o banco, então o(s) jogador(es) sem nenhuma riqueza deve(m) receber duas moedas do banco'],
         ['Burocracia', 'A ação Golpe de Estado mão pode ser efetuada durante esse turno(caso só seja possivel aplicar o Golpe de Estado, o mesmo deverá passar a vez)'],
         ['Pagando dívidas?', 'Você pode doar quantas moedas desejar a outro jogador, assim como receber durante esse turno'],
         ['Semana de festivais', 'Cada jogador deve receber uma moeda do banco, seguindo a ordem dos turnos'],
         ['Sussurros da corte', 'Escolha outro jogador, a menos que ele lhe pague uma moeda, olhe uma de suas influências e decida se ele deve mantê-la ou trocá-la'],
     ];
+
+    if(religiao == true){
+        deck.push(
+            ['Milagres têm preços','Cada jogador deve pagar uma moeda para o banco das igrejas.'],
+            ['Sussurros na sacristia','Você deve escolher entre converter-se ou converter outro jogador.'],
+            ['Um adepto exemplar','Você pode escolher outro jogador, ele deverá pagar uma moeda ao banco das igrejas.'],
+            ['Um adepto exemplar','Você pode escolher outro jogador, ele deverá pagar uma moeda ao banco das igrejas.'],
+            ['Um adepto exemplar','Você pode escolher outro jogador, ele deverá pagar uma moeda ao banco das igrejas.'],
+            ['Expiação','Você deve se converter.'],
+            ['Expiação','Você deve se converter.'],
+            ['Expiação','Você deve se converter.'],
+            ['Impondo a sua fé','Você deve converter outro jogador que seja de uma religião diferente da sua. Caso não haja, ignore'],
+            ['Impondo a sua fé','Você deve converter outro jogador que seja de uma religião diferente da sua. Caso não haja, ignore'],
+            ['Impondo a sua fé','Você deve converter outro jogador que seja de uma religião diferente da sua. Caso não haja, ignore']
+            );
+    }
+    return deck;
 }
-
-
-
-// console.log(lista[0][0]);
-
-// array.splice( index,)
 
 function sortearEvento(){
 
